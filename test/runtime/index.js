2ba25afaf79cd1a1db7a8e313596274662ddd8c8
Polina Soshnin <psoshnin@gmail.com>
2017-07-23 20:40:39 -0700

    test framework

--- test/runtime/index.js
+++ test/runtime/index.js
@@ -1,3 +1,50 @@
-class Test {}
+class Test {
+
+  constructor() {
+    this.suites = [];
+  }
+
+  describe(name, callback) {
+    this.suites.push(new Suite(name););
+    callback();
+  };
+
+  before(callback) {
+    currentSuite.before = callback;
+  };
+
+  after(callback) {
+    currentSuite.after = callback;
+  };
+
+  it(name, callback) {
+    currentSuite.tests.push({name: name, func: callback});
+  };
+
+  run() {
+    this.suites.forEach(function(suite) {
+      suite.before();
+      suite.tests.forEach(function(test) {
+        test.func();
+      });
+      suite.after();
+    });
+  };
+
+  currentSuite() {
+    return this.suites[this.suites.length-1];
+  }
+}
+
+class Suite {
+  constructor(name) {
+    this.name = name; 
+    this.tests = [];
+    this.before = null;
+    this.after = null;
+  }
+}
+
+const t = new Test();
 
 module.exports = Test;
